5cb5cb3fdd6370ef96235aa0e7a8e565d058eaec
KesaubeEire <760093121@qq.com>
2018-01-20 20:24:05 +0800

    fail to cure 树 update finally on 树

--- 数据结构_/006树结构/Program.cs
+++ 数据结构_/006树结构/Program.cs
@@ -11,7 +11,62 @@ namespace 树结构
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            /*Test
+             * 
+             * 
+            BiTree<int> nini = new BiTree<int>(0);
+            Node<int> l1 = new Node<int>(11);
+            Node<int> r1 = new Node<int>(12);
+            nini.Head.LChild = l1;
+            nini.Head.RChild = r1;
+            Node<int> r1l2 = r1.LChild = new Node<int>(21);
+            Node<int> r1l2r3 = r1l2.RChild = new Node<int>(32);
+            nini.PreOrder(nini.Head);
+            */
+            //  BiTree<int> nini = RandomGenerator_BitTree(100);
+            //   nini.PreOrder(nini.Head);
+            Console.WriteLine("233");
+            Console.ReadKey();
         }
+
+
+        static BiTree<int> RandomGenerator_BitTree(int randommax_numrange, int randommax_layer = 5)
+        {
+            int trigger = 0;
+            int RR(int max)
+            {
+                Random ran = new Random();
+                int a;
+                a = ran.Next(0, max);
+                return a;
+            }
+            void Regain(Node<int> root)
+            {
+                if (trigger == randommax_layer) return;
+                trigger++;
+                if (trigger <= randommax_layer)
+                    Regain(BiTree<int>.InsertL(RR(randommax_numrange), root));
+                trigger--;
+                if (trigger <= randommax_layer)
+                    Regain(BiTree<int>.InsertR(RR(randommax_numrange), root));
+            }
+            if (randommax_layer < 5 || randommax_layer > 11)
+            {
+                Console.WriteLine("no less than 5 layers and no more than 11 layers");
+                return default(BiTree<int>);
+            }
+            BiTree<int> beep = new BiTree<int>(RR(randommax_numrange));
+            Regain(beep.Head);
+
+            return beep;
+
+
+
+        }
+
+
+
     }
 
 
@@ -20,13 +75,11 @@ namespace 树结构
     /// </summary>
     class Node<T>
     {
-        private T data; //数据域
-        private Node<T> lChild;//左子树
-        private Node<T> rChild;//右子树
+        public T Data; //数据域
 
-        public T Data { get => data; set => data = value; }
-        internal Node<T> LChild { get => lChild; set => lChild = value; }
-        internal Node<T> RChild { get => rChild; set => rChild = value; }
+        public Node<T> LChild;//左子树
+        public Node<T> RChild;//右子树
+
 
         //========================CONSTRUCTION FIELD
 
@@ -66,35 +119,36 @@ namespace 树结构
 
     class BiTree<T>
     {
-        private Node<T> head;
+        public Node<T> Head;
 
-        internal Node<T> Head { get => head; set => head = value; }
+
 
         //========================CONSTRUCTION FIELD
         public BiTree()
         {
-            head = null;
+            Head = null;
         }
         public BiTree(T val)
         {
             Node<T> p = new Node<T>(val);
-            head = p;
+            Head = p;
         }
         public BiTree(T val, Node<T> lp, Node<T> rp)
         {
             Node<T> p = new Node<T>(val, lp, rp);
             //这个十分有趣，得学学这个方式，没必要另造两个字段，这就他妈够了，巧用之前造的轮子！
-            head = p;
+            Head = p;
         }
         //========================CONSTRUCTION FIELD
         //========================FUNCTION FIELD
+
         /// <summary>
         /// 判断是否是空二叉树
         /// </summary>
         /// <returns><c>true</c>, if empty was ised, <c>false</c> otherwise.</returns>
         public bool isEmpty()
         {
-            if (head == null) return true;//不理解为什么这里是head == null 而不是子节点
+            if (Head == null) return true;//不理解为什么这里是head == null 而不是子节点
             else return false;
         }
 
@@ -104,7 +158,7 @@ namespace 树结构
         /// <returns>The root.</returns>
         public Node<T> Root()
         {
-            return head;
+            return Head;
         }
 
         /// <summary>
@@ -126,46 +180,29 @@ namespace 树结构
             return p.RChild;
         }
         /// <summary>
-        /// 将节点p的左子树插入值为val的新节点，原来的左子树成为新节点的左子树
+        /// 将节点p的左子树插入值为val的新节点
         /// </summary>
         /// <param name="val">Value.</param>
         /// <param name="p">P.</param>
-        public void InsertL(T val, Node<T> p)
+        static public Node<T> InsertL(T val, Node<T> p)
         {
-            // p.LChild.Data = val;
-            // p.LChild.LChild = p.LChild;
-            //上面是我的方法
-            //下面是书上的方法
-            Node<T> tem = new Node<T>(val);
-            tem.LChild = p.LChild;
-            p.LChild = tem;
-            //他给的这个顺序对不对呢？
-            //我感觉是
-            /*
-             * tem.LChild = p.LChild;
-             * p.LChild = tem;
-             * 这个顺序啊！
-            */
-            ///--------------------修改认知ver0.1
-            /// 人家是对的，这个功能相当于在中间多插入了一层，人家的是对的
+            return p.LChild = new Node<T>(val);
         }
         /// <summary>
-        /// 将节点p的右子树插入值为val的新节点，原来的右子树成为新节点的右子树
+        /// 将节点p的右子树插入值为val的新节点
         /// </summary>
         /// <param name="val">Value.</param>
         /// <param name="p">P.</param>
-        public void InsertR(T val, Node<T> p)
+        static public Node<T> InsertR(T val, Node<T> p)
         {
-            Node<T> tem = new Node<T>(val);
-            tem.RChild = p.RChild;
-            p.RChild = tem;
+            return p.RChild = new Node<T>(val);
         }
         /// <summary>
         /// 若节点不为空，则删除该节点的左子树
         /// </summary>
         /// <returns>The l.</returns>
         /// <param name="p">P.</param>
-        public Node<T> DeleteL(Node<T> p)
+        static public Node<T> DeleteL(Node<T> p)
         {
             if ((p == null) || (p.LChild == null))
             {
@@ -180,7 +217,7 @@ namespace 树结构
         /// </summary>
         /// <returns>The r.</returns>
         /// <param name="p">P.</param>
-        public Node<T> DeleteR(Node<T> p)
+        static public Node<T> DeleteR(Node<T> p)
         {
             if ((p == null) || (p.RChild == null))
             {
@@ -195,12 +232,15 @@ namespace 树结构
         /// </summary>
         /// <returns><c>true</c>, if leaf was ised, <c>false</c> otherwise.</returns>
         /// <param name="p">P.</param>
-        public bool isLeaf(Node<T> p)
+        static public bool isLeaf(Node<T> p)
         {
             if (p != null && p.LChild == null && p.RChild == null) return true;
             else return false;
         }
         //========================FUNCTION FIELD
+        //========================STATIC FUNCTION FIELD
+
+        //========================STATIC FUNCTION FIELD
         //========================ESSENTIAL FUNCTION FIELD - 遍历二叉树的方法
 
         ///先序遍历，若二叉树为空，则结束遍历，否则：
@@ -215,10 +255,18 @@ namespace 树结构
             {
                 Console.Write("dataContent:\t" + root.Data);
                 Console.WriteLine("\tline:\t" + id_Line_Pre);
-                id_Line_Pre++;
-                PreOrder(root.LChild);
-                id_Line_Pre--;
-                PreOrder(root.RChild);
+                if (root.LChild != null)
+                {
+                    id_Line_Pre++;
+                    PreOrder(root.LChild);
+                    id_Line_Pre--;
+                }
+                if (root.RChild != null)
+                {
+                    id_Line_Pre++;
+                    PreOrder(root.RChild);
+                    id_Line_Pre--;
+                }
 
             }
         }
@@ -263,16 +311,17 @@ namespace 树结构
             }
         }
 
-        ///层次遍历，
+        ///层次遍历，我自己写的，完了对比对比能不能用
         /// 
         /// 
         /// 
 
         //设置一个队列装载所有的顺序队列
         //这招真他妈好用
-        private Queue<Node<T>> sq = new Queue<Node<T>>(1000);
-        public void LevelOrder(Node<T> root)
+        public void LevelOrder_Mine(Node<T> root)
         {
+            int trigger = 0;
+            Queue<Node<T>> sq = new Queue<Node<T>>(1000);
             if (root == null) return;
             else
             {
@@ -281,19 +330,43 @@ namespace 树结构
 
                 if (root.LChild != null)
                 {
-                    sq.
+                    sq.Enqueue(root.LChild);
+                    trigger++;
                 }
                 if (root.RChild != null)
                 {
-                    LevelOrder(root.RChild);
+                    sq.Enqueue(root.RChild);
+                    trigger++;
                 }
+            }
+            if (trigger != 0)
+            {
 
-
-
-
+                for (int i = trigger; i > 0; i--)
+                {
+                    LevelOrder_Mine(sq.Dequeue());
+                }
             }
 
+        }
 
+        public void LevelOrder_Teacher(Node<T> root)
+        {
+            Queue<Node<T>> sq = new Queue<Node<T>>(500);
+            //根节点入队
+            sq.Enqueue(root);
+            //队列非空，节点没有处理完
+            while (sq.Count != 0)
+            {
+                //节点出队
+                Node<T> tmp = sq.Dequeue();
+                //处理当前节点
+                Console.Write("dataContent:\t" + tmp.Data);
+                //将当前的节点的左孩子入队
+                if (tmp.LChild != null) sq.Enqueue(tmp.LChild);
+                //将当前的节点的右孩子入队
+                if (tmp.RChild != null) sq.Enqueue(tmp.RChild);
+            }
         }
 
 
